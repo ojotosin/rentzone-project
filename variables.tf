1e909b5afec0ec8113093ccfa439cce3557dd7ad
Tosin Ojo <ojotosin1900@gmail.com>
2023-02-16 14:42:07 -0500

    adding vpc and natgw file

--- variables.tf
+++ variables.tf
@@ -16,3 +16,157 @@ variable "environment" {
     type = string
   
 }
+
+
+# vpc variables
+variable "dev_vpc_cidr" {
+    default             = "10.0.0.0/16"
+    description         = "dev vp cidr"
+    type                = string
+  
+}
+
+variable "public_subnetAZ1_cidr" {
+    default             = "10.0.0.0/24"
+    description         = "public subnetAZ1 cidr"
+    type                = string
+  
+}
+
+variable "public_subnetAZ2_cidr" {
+    default             = "10.0.1.0/24"
+    description         = "public subnetAZ2 cidr"
+    type                = string
+  
+}
+
+variable "public_rtb_cidr" {
+    default             = "0.0.0.0/0"
+    description         = "public routetable cidr"
+    type                = string
+  
+}
+
+variable "private_appsubnetAZ1_cidr" {
+    default             = "10.0.2.0/24"
+    description         = "private appsubnetAZ1 cidr"
+    type                = string
+  
+}
+
+variable "private_appsubnetAZ2_cidr" {
+    default             = "10.0.3.0/24"
+    description         = "private appsubnetAZ2 cidr"
+    type                = string
+  
+}
+
+variable "private_datasubnetAZ1_cidr" {
+    default             = "10.0.4.0/24"
+    description         = "private datasubnetAZ1 cidr"
+    type                = string
+  
+}
+
+variable "private_datasubnetAZ2_cidr" {
+    default             = "10.0.5.0/24"
+    description         = "private datasubnetAZ2 cidr"
+    type                = string
+  
+}
+
+# security group variables
+variable "ssh_location" {
+    default             = "0.0.0.0/0"
+    description         = "ip from which ssh is allowed"
+    type                = string
+  
+}
+
+# rds variables
+variable "database_snapshot_identifier" {
+    default             = "arn:aws:rds:us-east-1:774443160673:snapshot:fleetcart-final-snapshot"
+    description         = "the database snapshot arn"
+    type                = string
+  
+}
+
+variable "database_instance_class" {
+    default             = "db.t2.micro"
+    description         = "the database instance type"
+    type                = string
+  
+}
+
+variable "database_instance_identifier" {
+    default             = "dev-db-1"
+    description         = "the database instance/ cluster Name"
+    type                = string
+  
+}
+
+variable "multi_az_deployment" {
+    default             = false
+    description         = "creates a standby db instance"
+    type                = bool
+  
+}
+
+# application Load balancer variables
+variable "ssl_certificate_arn" {
+    default             = "arn:aws:acm:us-east-1:774443160673:certificate/3a4ae261-bb4e-45d6-a6fa-fafdea2466c1"
+    description         = "SSL certificate arn"
+    type                = string
+  
+}
+
+# SNS topic variables
+variable "operator_email" {
+    default             = "ojotosin1900@gmail.com"
+    description         = "my email address"
+    type                = string
+  
+}
+
+# autoscaling group variables
+variable "launch_template_name" {
+    default             = "dev-launch-template"
+    description         = "launch template name"
+    type                = string
+  
+}
+
+variable "ec2_image_id" {
+    default             = "ami-0b5ada25b9ba41857" // ami id from snapshot created
+    description         = "ami id"
+    type                = string
+  
+}
+
+variable "ec2_instance_type" {
+    default             = "t2.micro"
+    description         = "ec2 instance type"
+    type                = string
+  
+}
+
+variable "ec2_keypair_name" {
+    default             = "npop-Keypair"
+    description         = "key pair name"
+    type                = string
+  
+}
+
+# route 53 variables
+variable "domain_name" {
+    default             = "expressjapa.com"
+    description         = "domain name of the website"
+    type                = string
+  
+}
+ variable "record_name" {
+    default             = "www"
+    description         = "sub domain name"
+    type                = string
+   
+ }
---
+++ vpc.tf
@@ -0,0 +1,120 @@
+# creates the vpc
+resource "aws_vpc" "vpc" {
+  cidr_block                = var.dev_vpc_cidr
+  instance_tenancy          = "default"
+  enable_dns_hostnames      = true
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-vpc"
+  }
+}
+
+# creates the internet gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id                    = aws_vpc.vpc.id
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-igw"
+  }
+}
+
+# use data source to get all avalablility zones in region
+data "aws_availability_zones" "available_zones" {}
+
+# creates public subnet in az1
+resource "aws_subnet" "public_subnetAZ1" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.public_subnetAZ1_cidr
+  availability_zone         = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch   = true
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-public-subnetAZ1"
+  }
+} 
+
+# creates public subnet in az2
+resource "aws_subnet" "public_subnetAZ2" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.public_subnetAZ2_cidr
+  availability_zone         = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch   = true
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-public-subnetAZ2"
+  }
+}
+
+# creates route table
+resource "aws_route_table" "public_route_table" {
+  vpc_id                    = aws_vpc.vpc.id
+
+  route {
+    cidr_block              = var.public_rtb_cidr
+    gateway_id              = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-public-rtb"
+  }
+}
+
+# associates public subnet AZ1 with public route table
+resource "aws_route_table_association" "AZ1" {
+  subnet_id                 = aws_subnet.public_subnetAZ1.id
+  route_table_id            = aws_route_table.public_route_table.id
+}
+
+# associates public subnet AZ2 with public route table
+resource "aws_route_table_association" "AZ2" {
+  subnet_id                 = aws_subnet.public_subnetAZ2.id
+  route_table_id            = aws_route_table.public_route_table.id
+}
+
+# creates private app subnet in az1
+resource "aws_subnet" "private_appsubnetAZ1" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.private_appsubnetAZ1_cidr
+  availability_zone         = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch   = false
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-private-appsubnetAZ1"
+  }
+}
+
+# creates private app subnet in az2
+resource "aws_subnet" "private_appsubnetAZ2" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.private_appsubnetAZ2_cidr
+  availability_zone         = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch   = false
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-private-appsubnetAZ2"
+  }
+}
+
+# creates private data subnet in az1
+resource "aws_subnet" "private_datasubnetAZ1" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.private_datasubnetAZ1_cidr
+  availability_zone         = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch   = false
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-private-datasubnetAZ1"
+  }
+}
+
+# creates private data subnet in az2
+resource "aws_subnet" "private_datasubnetAZ2" {
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.private_datasubnetAZ2_cidr
+  availability_zone         = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch   = false
+
+  tags                      = {
+    Name                    = "${var.project_name}-${var.environment}-private-datasubnetAZ2"
+  }
+}
